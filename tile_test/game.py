a1d75e42ddf23114bd76e8d677c9eb74c6568b60
Jeff DeWall <sr.jilarious@fastmail.net>
2021-06-02 20:34:39 -0600

    Added player tile collisions with level1 main_layer tiles.

--- tile_test/game.py
+++ tile_test/game.py
@@ -14,7 +14,9 @@ ScreenWidth = 768
 ScreenHeight = 432
 PlayerStartX = 25
 PlayerStartY = 25
-Speed = 10
+PlayerWidth = 16
+PlayerHeight = 16
+Speed = 2
 
 class GameContext:
     def __init__(self):
@@ -24,49 +26,63 @@ class GameContext:
 
         self.font = pygame.font.Font(None, 15)
         self.running = True
-        self.player_x = PlayerStartX
-        self.player_y = PlayerStartY
-        self.player_radius = 8
-        self.player_radius_sq = self.player_radius**2
-        self.dot_radius = 4
-        self.dot_radius_sq = self.dot_radius**2
         self.score = 1234
-        self.dots = []
-
+        
+        self.player_rect = pygame.Rect(PlayerStartX, PlayerStartY, PlayerWidth, PlayerHeight)
         self.tmxdata = load_pygame("level1.tmx")
         self.map_data = pyscroll.TiledMapData(self.tmxdata)
-        self.map_layer = pyscroll.BufferedRenderer(self.map_data, (ScreenWidth, ScreenHeight))
+        self.main_tiles = self.tmxdata.get_layer_by_name("main_layer")
+        
+        self.map_layer = pyscroll.BufferedRenderer(self.map_data, (ScreenWidth, ScreenHeight), clamp_camera=False)
         self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer)
         if pygame.joystick.get_count() > 0:
             self.joystick = joystick = pygame.joystick.Joystick(0)
             self.joystick.init()
         else:
             self.joystick = None
-    def create_dot(self):
-        if len(self.dots) < 5000:
-            self.dots += [(random.randint(0, ScreenWidth), random.randint(0, ScreenHeight))]
-
-    def check_collide(self, dot):
-        dist_sq = (dot[0] - self.player_x)**2 + (dot[1] - self.player_y)**2
-        if dist_sq < (self.dot_radius_sq + self.player_radius_sq):
-            return True
-        return False
-
+        self.debug_rects = []
 
     def move_up(self):
-        self.player_y = max(self.player_radius, self.player_y - Speed)
-
+        # self.player_y = max(self.player_radius, self.player_y - Speed)
+        new_rect = self.player_rect.move(0, -Speed)
+        ty = int(new_rect.top / self.tmxdata.tileheight)
+        tx_l = int(new_rect.left / self.tmxdata.tilewidth)
+        tx_r = int(new_rect.right / self.tmxdata.tilewidth)
+        if not self.check_collide_tile(new_rect, tx_l, ty) and not self.check_collide_tile(new_rect, tx_r, ty):
+            self.player_rect = new_rect
+        
     def move_down(self):
-        self.player_y = min(self.player_y + Speed,  10*ScreenHeight - self.player_radius)
+        new_rect = self.player_rect.move(0, Speed)
+        ty = int(new_rect.bottom / self.tmxdata.tileheight)
+        tx_l = int(new_rect.left / self.tmxdata.tilewidth)
+        tx_r = int(new_rect.right / self.tmxdata.tilewidth)
+        if not self.check_collide_tile(new_rect, tx_l, ty) and not self.check_collide_tile(new_rect, tx_r, ty):
+            self.player_rect = new_rect
 
     def move_left(self):
-        self.player_x = max(self.player_radius, self.player_x - Speed)
+        new_rect = self.player_rect.move(-Speed, 0)
+        tx = int(new_rect.left / self.tmxdata.tilewidth)
+        ty_t = int(new_rect.top / self.tmxdata.tileheight)
+        ty_b = int(new_rect.bottom / self.tmxdata.tileheight)
+        if not self.check_collide_tile(new_rect, tx, ty_t) and not self.check_collide_tile(new_rect, tx, ty_b):
+            self.player_rect = new_rect
 
     def move_right(self):
-        self.player_x = min(self.player_x + Speed, 10*ScreenWidth - self.player_radius)
+        new_rect = self.player_rect.move(Speed, 0)
+        tx = int(new_rect.right / self.tmxdata.tilewidth)
+        ty_t = int(new_rect.top / self.tmxdata.tileheight)
+        ty_b = int(new_rect.bottom / self.tmxdata.tileheight)
+        if not self.check_collide_tile(new_rect, tx, ty_t) and not self.check_collide_tile(new_rect, tx, ty_b):
+            self.player_rect = new_rect
 
     def update_game(self):
         pressed_keys = pygame.key.get_pressed()
+
+        if (pressed_keys[K_UP] or 
+            pressed_keys[K_DOWN] or
+            pressed_keys[K_LEFT] or
+            pressed_keys[K_RIGHT]):
+            self.debug_rects.clear()
 
         if pressed_keys[K_UP]:
             self.move_up()
@@ -86,7 +102,7 @@ class GameContext:
         if pressed_keys[K_ESCAPE]:
             self.running = False
 
-
+        # Handle joystick input
         if self.joystick is not None:
             jx, jy = self.joystick.get_hat(0)
             if jy > 0:
@@ -98,25 +114,41 @@ class GameContext:
             if jx > 0:
                 self.move_right()
 
-        # Check collisions with dots
-        for i, dot in enumerate(self.dots):
-            if self.check_collide(dot):
-                self.score += 50
-                del self.dots[i]
+        
+    def check_collide_tile(self, player_rect, x, y):
+        tile = self.get_main_tile(x, y)
+        tw = self.tmxdata.tilewidth
+        th = self.tmxdata.tileheight
+        if tile != 0:
+            collide_rect = pygame.Rect(int(x*tw), int(y*th), tw, th)
+            # Debug, draw collide rect.
+            self.debug_rects.append(collide_rect)
+
+            return player_rect.colliderect(collide_rect)
+        else:
+            return False
+
+    def get_main_tile(self, x, y):
+        return self.main_tiles.data[int(y)][int(x)]
 
     def render(self):
         # Fill the background with white
         # self.off_screen.fill((0,0,0))
 
+
+        self.group.center((self.player_rect.x, self.player_rect.y))
+        #self.group.center((ScreenWidth/2, ScreenHeight/2))
+        self.group.draw(self.off_screen)
+        
         # Draw a solid blue circle in the center
         # pygame.draw.circle(self.off_screen, (0, 0, 255), (self.player_x, self.player_y), self.player_radius)
+        p_screen_pos = pygame.Rect(ScreenWidth/2, ScreenHeight/2, PlayerWidth, PlayerHeight)
+        pygame.draw.rect(self.off_screen, (0,0,255), p_screen_pos)
+        #pygame.draw.rect(self.off_screen, (0,0,255), self.player_rect)
 
-        # Draw the dots
-        # for i, dot in enumerate(self.dots):
-        #     pygame.draw.circle(self.off_screen, (255, 255, 0), (dot[0], dot[1]), self.dot_radius)
+        # for r in self.debug_rects:
+        #     pygame.draw.rect(self.off_screen, (255,255,255), r.move(p_screen_pos.topleft), width=1)
 
-        self.group.center((self.player_x, self.player_y))
-        self.group.draw(self.off_screen)
         txt = self.font.render("Score: " + str(self.score), False, pygame.Color('white'))
         self.off_screen.blit(txt, (5, 5))
 
@@ -130,9 +162,6 @@ if __name__ == "__main__":
     pygame.joystick.init()
 
     context = GameContext()
-    
-    for i in range(100):
-        context.create_dot()
 
     while context.running:
         # Did the user click the window close button?
