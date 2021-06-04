56a53eb1fc322ccc7c934514deebaae02c87ea4c
Jeff DeWall <sr.jilarious@fastmail.net>
2021-06-03 20:43:35 -0600

    Added player to group to get layering working.

--- tile_test/game.py
+++ tile_test/game.py
@@ -12,11 +12,21 @@ WindowWidth = 1920
 WindowHeight = 1080
 ScreenWidth = 768
 ScreenHeight = 432
-PlayerStartX = 25
-PlayerStartY = 25
-PlayerWidth = 16
-PlayerHeight = 16
+PlayerStartX = 64
+PlayerStartY = 64
+PlayerWidth = 32
+PlayerHeight = 32
 Speed = 2
+
+pygame.init()
+BluePiece = pygame.image.load("blue_piece.png")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(BluePiece, (PlayerWidth, PlayerHeight))
+        self.rect = pygame.Rect(PlayerStartX, PlayerStartY, PlayerWidth, PlayerHeight)
+        self.layer = 0
 
 class GameContext:
     def __init__(self):
@@ -28,13 +38,16 @@ class GameContext:
         self.running = True
         self.score = 1234
         
-        self.player_rect = pygame.Rect(PlayerStartX, PlayerStartY, PlayerWidth, PlayerHeight)
+        self.player = Player()
+        
         self.tmxdata = load_pygame("level1.tmx")
         self.map_data = pyscroll.TiledMapData(self.tmxdata)
         self.main_tiles = self.tmxdata.get_layer_by_name("main_layer")
         
         self.map_layer = pyscroll.BufferedRenderer(self.map_data, (ScreenWidth, ScreenHeight), clamp_camera=False)
         self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer)
+        self.group.add(self.player)
+
         if pygame.joystick.get_count() > 0:
             self.joystick = joystick = pygame.joystick.Joystick(0)
             self.joystick.init()
@@ -44,36 +57,36 @@ class GameContext:
 
     def move_up(self):
         # self.player_y = max(self.player_radius, self.player_y - Speed)
-        new_rect = self.player_rect.move(0, -Speed)
+        new_rect = self.player.rect.move(0, -Speed)
         ty = int(new_rect.top / self.tmxdata.tileheight)
         tx_l = int(new_rect.left / self.tmxdata.tilewidth)
         tx_r = int(new_rect.right / self.tmxdata.tilewidth)
         if not self.check_collide_tile(new_rect, tx_l, ty) and not self.check_collide_tile(new_rect, tx_r, ty):
-            self.player_rect = new_rect
+            self.player.rect = new_rect
         
     def move_down(self):
-        new_rect = self.player_rect.move(0, Speed)
+        new_rect = self.player.rect.move(0, Speed)
         ty = int(new_rect.bottom / self.tmxdata.tileheight)
         tx_l = int(new_rect.left / self.tmxdata.tilewidth)
         tx_r = int(new_rect.right / self.tmxdata.tilewidth)
         if not self.check_collide_tile(new_rect, tx_l, ty) and not self.check_collide_tile(new_rect, tx_r, ty):
-            self.player_rect = new_rect
+            self.player.rect = new_rect
 
     def move_left(self):
-        new_rect = self.player_rect.move(-Speed, 0)
+        new_rect = self.player.rect.move(-Speed, 0)
         tx = int(new_rect.left / self.tmxdata.tilewidth)
         ty_t = int(new_rect.top / self.tmxdata.tileheight)
         ty_b = int(new_rect.bottom / self.tmxdata.tileheight)
         if not self.check_collide_tile(new_rect, tx, ty_t) and not self.check_collide_tile(new_rect, tx, ty_b):
-            self.player_rect = new_rect
+            self.player.rect = new_rect
 
     def move_right(self):
-        new_rect = self.player_rect.move(Speed, 0)
+        new_rect = self.player.rect.move(Speed, 0)
         tx = int(new_rect.right / self.tmxdata.tilewidth)
         ty_t = int(new_rect.top / self.tmxdata.tileheight)
         ty_b = int(new_rect.bottom / self.tmxdata.tileheight)
         if not self.check_collide_tile(new_rect, tx, ty_t) and not self.check_collide_tile(new_rect, tx, ty_b):
-            self.player_rect = new_rect
+            self.player.rect = new_rect
 
     def update_game(self):
         pressed_keys = pygame.key.get_pressed()
@@ -144,17 +157,19 @@ class GameContext:
         # self.off_screen.fill((0,0,0))
 
 
-        self.group.center((self.player_rect.x, self.player_rect.y))
+        self.group.center((self.player.rect.x, self.player.rect.y))
         #self.group.center((ScreenWidth/2, ScreenHeight/2))
+        
         self.group.draw(self.off_screen)
         
         # Draw a solid blue circle in the center
         # pygame.draw.circle(self.off_screen, (0, 0, 255), (self.player_x, self.player_y), self.player_radius)
-        p_screen_pos = pygame.Rect(ScreenWidth/2, ScreenHeight/2, PlayerWidth, PlayerHeight)
-        pygame.draw.rect(self.off_screen, (0,0,255), p_screen_pos)
-        #pygame.draw.rect(self.off_screen, (0,0,255), self.player_rect)
+        # p_screen_pos = pygame.Rect(ScreenWidth/2, ScreenHeight/2, PlayerWidth, PlayerHeight)
+        # pygame.draw.rect(self.off_screen, (0,0,255), p_screen_pos)
 
-        map_offset = (self.player_rect.x-ScreenWidth/2, self.player_rect.y-ScreenHeight/2)
+        #pygame.draw.rect(self.off_screen, (0,0,255), self.player.rect)
+
+        map_offset = (self.player.rect.x-ScreenWidth/2, self.player.rect.y-ScreenHeight/2)
         for r in self.debug_rects:
             pygame.draw.rect(self.off_screen, (255,255,255), r.move((-map_offset[0], -map_offset[1])), width=1)
 
@@ -167,7 +182,7 @@ class GameContext:
 
 
 if __name__ == "__main__":
-    pygame.init()
+    
     pygame.joystick.init()
 
     context = GameContext()
