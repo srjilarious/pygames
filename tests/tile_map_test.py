5cd9e7c8d7c28930f0621f915ea424868636cccc
Jeff DeWall <sr.jilarious@fastmail.net>
2021-07-25 20:18:49 -0600

    Added test that we can read tile data from map layer.

--- tests/tile_map_test.py
+++ tests/tile_map_test.py
@@ -6,7 +6,7 @@ import arcade
 SCRIPT_ROOT = os.path.dirname(os.path.realpath(__file__))
 ASSETS_DIR = os.path.join(SCRIPT_ROOT, "..", "assets")
 
-def test_can_read_map_data():
+def test_can_read_map_properties():
     # Read in the tiled map
     my_map = arcade.tilemap.read_tmx(os.path.join(ASSETS_DIR, "level1.tmx"))
 
@@ -14,3 +14,20 @@ def test_can_read_map_data():
     assert my_map.properties["player_start_x"] == '3'
     assert my_map.properties["player_start_y"] == '4'
     assert my_map.properties["start_script"] == "assets.scripts.level1"
+
+def test_can_read_map_data():
+    # Read in the tiled map
+    my_map = arcade.tilemap.read_tmx(os.path.join(ASSETS_DIR, "level1.tmx"))
+
+    l = arcade.tilemap.get_tilemap_layer(my_map, "main_layer")
+
+    # Check the first few tiles are what we expect
+    assert l.layer_data[0][0] == 100 # left wall
+    assert l.layer_data[0][1] == 0  # empty
+    assert l.layer_data[0][2] == 63 # tree top
+    
+    # Check another line is also what we expect.
+    assert l.layer_data[7][0] == 100 # left wall
+    assert l.layer_data[7][1] == 3   # grass tile
+    assert l.layer_data[7][2] == 3   # grass tile
+
