92984d0af489dad9db69f62a2a83dfe77b1fbf0d
Jeff DeWall <sr.jilarious@fastmail.net>
2021-07-25 20:07:22 -0600

    Fixed test (mostly by forcing pytiled_parser 0.9.4

--- tests/tile_map_test.py
+++ tests/tile_map_test.py
@@ -11,6 +11,6 @@ def test_can_read_map_data():
     my_map = arcade.tilemap.read_tmx(os.path.join(ASSETS_DIR, "level1.tmx"))
 
     # Check for map properties
-    assert my_map.properties["player_start_x"] == 3
-    assert my_map.properties["player_start_y"] == 4
+    assert my_map.properties["player_start_x"] == '3'
+    assert my_map.properties["player_start_y"] == '4'
     assert my_map.properties["start_script"] == "assets.scripts.level1"
