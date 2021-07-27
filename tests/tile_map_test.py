b94c17d112c3dd896b289810f1bbdc3ebf858ae6
Jeff DeWall <sr.jilarious@fastmail.net>
2021-07-26 19:10:31 -0600

    Added test that we can read object data form maps.

--- tests/tile_map_test.py
+++ tests/tile_map_test.py
@@ -2,6 +2,7 @@ import os
 
 import pytest
 import arcade
+import pytiled_parser
 
 SCRIPT_ROOT = os.path.dirname(os.path.realpath(__file__))
 ASSETS_DIR = os.path.join(SCRIPT_ROOT, "..", "assets")
@@ -31,3 +32,27 @@ def test_can_read_map_data():
     assert l.layer_data[7][1] == 3   # grass tile
     assert l.layer_data[7][2] == 3   # grass tile
 
+def test_can_read_object_data():
+    # Read in the tiled map
+    my_map = arcade.tilemap.read_tmx(os.path.join(ASSETS_DIR, "level1.tmx"))
+
+    l = arcade.tilemap.get_tilemap_layer(my_map, "collidable_objects")
+
+    assert isinstance(l, pytiled_parser.objects.ObjectLayer)
+
+    assert len(l.tiled_objects) == 6
+
+    # Note, objects are stored in id order, which was descending in map editor.
+    assert l.tiled_objects[5].name == "checkpoint_2"
+    assert l.tiled_objects[4].name == "checkpoint_1a"
+
+    assert l.tiled_objects[3].name == "change_to_level_2"
+    assert l.tiled_objects[3].type == "changeLevel"
+    assert l.tiled_objects[3].properties["level_name"] == "Content/level2.tmx"
+    
+    assert l.tiled_objects[2].name == "secret_sound"
+    assert l.tiled_objects[2].type == "run_script"
+    assert l.tiled_objects[2].properties["script"] == "Content/play_secret_cue.lua"
+
+    assert l.tiled_objects[1].name == "checkpoint_1b"
+    assert l.tiled_objects[0].name == "checkpoint_3"
