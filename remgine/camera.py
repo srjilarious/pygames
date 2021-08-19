03cc3600f960ed94404c291165e4eecf4b11c105
Jeff DeWall <sr.jilarious@fastmail.net>
2021-08-19 16:55:29 -0600

    Added linear slowdown with max velocity to camera module.

--- remgine/camera.py
+++ remgine/camera.py
@@ -2,8 +2,11 @@
 class Camera:
     def __init__(self, context):
         self.context = context
-        self._max_vel_x = 3
-        self._max_vel_y = 3
+
+        self._max_vel_x = 8
+        self._max_vel_y = 8
+
+        self.small_dist = 150
 
         self.goal_x = 0
         self.goal_y = 0
@@ -14,9 +17,22 @@ class Camera:
 
         dist_x = self.goal_x - curr_x
         dist_y = self.goal_y - curr_y
+        adx = abs(dist_x)
+        ady = abs(dist_y)
 
-        amt_x = min(self._max_vel_x, abs(dist_x))
-        amt_y = min(self._max_vel_y, abs(dist_y))
+        if adx < self.small_dist:
+            mx = (adx * self._max_vel_x // self.small_dist)
+        else:
+            mx = self._max_vel_x
+        
+        if ady < self.small_dist:
+            my = (ady * self._max_vel_y // self.small_dist)
+        else:
+            my = self._max_vel_y
+        
+
+        amt_x = min(mx, adx)
+        amt_y = min(my, ady)
         if dist_x < 0:
             amt_x = -amt_x
         
